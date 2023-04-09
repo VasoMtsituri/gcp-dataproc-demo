2d398e612ea53362e52bda29cb624c8457fff655
Vasiko Mtsituri <vasiko.mtsituri@makingscience.com>
2023-04-09 22:58:12 +0400

    feat(demo1): implement Hive table reader function; Refactor code

--- spark_demo.py
+++ spark_demo.py
@@ -1,7 +1,21 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 
-print('Storing random numbers in a hive table')
-spark.range(100).write.saveAsTable("random_numbers")
-print('complete')
+def main():
+    spark = SparkSession.builder.enableHiveSupport().getOrCreate()
+
+    print('Storing random numbers in a hive table')
+    # spark.range(100).write.saveAsTable("random_numbers")
+    print('complete')
+
+    read_hive(spark_session=spark, table_name='random_numbers')
+
+
+def read_hive(spark_session, table_name):
+    table = spark_session.sql(f'SELECT * FROM {table_name}')
+
+    table.show()
+
+
+if __name__ == '__main__':
+    main()
