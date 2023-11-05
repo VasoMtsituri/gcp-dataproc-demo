93e5cab61ddd64d92b6aea84a39d14c888f235f6
Vasiko Mtsituri <vasiko.mtsituri@makingscience.com>
2023-11-05 10:16:53 +0400

    chore: change writing final df from BQ to local CSV file

--- joins_in_spark/inner_join_bq_tables.py
+++ joins_in_spark/inner_join_bq_tables.py
@@ -16,15 +16,13 @@ FINAL_TABLE = 'final_table'
 
 spark = SparkSession \
     .builder \
-    .appName('spark-bigquery-demo') \
-    .config('spark.jars.packages', 'com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.23.2') \
-    .config("spark.jars", "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar") \
+    .appName('spark-bigquery-demo-local1') \
+    .config('spark.jars', '/home/vaso/PycharmProjects/gcp-dataproc-demo/connectors/spark-bigquery-with-dependencies_2.12-0.16.1.jar') \
     .getOrCreate()
 
 # Use the Cloud Storage bucket for temporary BigQuery export data used
 # by the connector.
 spark.conf.set('temporaryGcsBucket', TEMP_BUCKET)
-spark.conf.set("spark.sql.repl.eagerEval.enabled", True)
 
 
 def main():
@@ -42,13 +40,15 @@ def main():
     df.show()
 
     default_timestamp = datetime.utcnow().isoformat()
-    df = df.withColumn("new_column", lit(default_timestamp))
+    df = df.withColumn("timestamp", lit(default_timestamp))
     df.show()
 
-    # Save the data to BigQuery
-    df.write.format('bigquery') \
-        .option('table', f'{PROJECT_ID}:{DATASET}.{FINAL_TABLE}') \
-        .save()
+    # # Save the data to BigQuery
+    # df.write.format('bigquery') \
+    #     .option('table', f'{PROJECT_ID}:{DATASET}.{FINAL_TABLE}') \
+    #     .save()
+
+    df.write.csv(f'{FINAL_TABLE}.csv', header=True, mode='overwrite')
 
 
 if __name__ == '__main__':
